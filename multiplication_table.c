27849a6d4f681b78adb1897cdc086428d5089e13
Hassam <144311429+PapaGator0012@users.noreply.github.com>
2023-10-22 11:55:33 -0700

    table setting up input outsput seq

---
+++ multiplication_table.c
@@ -0,0 +1,31 @@
+
+// Author : Syed Hassam Jan 
+// Date :   10/22/23
+
+
+
+
+
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+int num;
+
+printf("enter a number ");
+scanf("%d",&num);
+
+}
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
